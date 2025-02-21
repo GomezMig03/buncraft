08c40aff1a35efe1a9073e21b8cd3f6c0bbc26a1
Miguel <gomezmig03@gmail.com>
2025-02-22 00:10:38 +0100

    Add pong response and refactor

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
-import { toVarInt, toUshort } from "./utils/types";
+import { asVarInt, asUshort, toVarInt } from "./utils/types"
 
-console.log("Server is waiting for players...");
+console.log("Server is waiting for players...")
 
 Bun.listen({
   hostname: "127.0.0.1",
   port: 8080,
   socket: {
     data(socket, data) {
-      let buffer = Buffer.from(data);
-      const byteArray = Array.from(buffer);
-      console.log(byteArray);
+      let buffer = Buffer.from(data)
+      const byteArray = Array.from(buffer)
 
-      const hand = convertHandshake(byteArray);
-      hand.show();
+      if (byteArray[0] === 1) {
+        socket.write(data) // pong
+        return 
+      }
+
+      const hand = convertHandshake(byteArray)
+      hand.show()
     },
     open(socket) {
-      console.log(`${socket} socket opened`);
+      console.log(`${socket} socket opened`)
     },
     close(socket) {
-      console.log(`${socket} close`);
+      console.log(`${socket} close`)
     },
     error(socket, error) {
-      console.log(`${socket} Error in socket: ${JSON.stringify(error)}`);
+      console.log(`${socket} Error in socket: ${JSON.stringify(error)}`)
     },
   },
-});
+})
 
 const convertHandshake = (byteArray) => {
-  const size = byteArray[0];
-  const id = byteArray[1];
+  const size = byteArray[0]
+  const id = byteArray[1]
   if (size < 12 && id != 0) {
-    return;
+    return
   }
 
-  const protocolVersion = toVarInt([byteArray[2], byteArray[3]]);
+  const protocolVersion = asVarInt([byteArray[2], byteArray[3]])
 
-  const stringLength = byteArray[4];
+  const stringLength = byteArray[4]
 
-  const ip = [];
+  const ip = []
 
   for (let i = 5; i < stringLength + 5; i++) {
-    ip.push(byteArray[i]);
+    ip.push(byteArray[i])
   }
 
-  const ipBuffer = Buffer.from(ip);
-  const ipString = ipBuffer.toString("utf-8");
+  const ipBuffer = Buffer.from(ip)
+  const ipString = ipBuffer.toString("utf-8")
 
-  const portString = toUshort([
+  const portString = asUshort([
     byteArray[stringLength + 5],
     byteArray[stringLength + 6],
-  ]);
+  ])
 
-  const nextState = byteArray[byteArray.length - 1];
+  const nextState = byteArray[byteArray.length - 1]
 
   return new Handshake(
     size,
@@ -61,27 +65,27 @@ const convertHandshake = (byteArray) => {
     ipString,
     portString,
     nextState,
-  );
-};
+  )
+}
 
 class Handshake {
   constructor(size, id, protocolVersion, stringLength, ip, port, nextState) {
-    this.size = size;
-    this.id = id;
-    this.protocolVersion = protocolVersion;
-    this.stringLength = stringLength;
-    this.ip = ip;
-    this.port = port;
+    this.size = size
+    this.id = id
+    this.protocolVersion = protocolVersion
+    this.stringLength = stringLength
+    this.ip = ip
+    this.port = port
     switch (nextState) {
       case 1:
-        this.nextState = "status";
-        break;
+        this.nextState = "status"
+        break
       case 2:
-        this.nextState = "login";
-        break;
+        this.nextState = "login"
+        break
       default:
-        this.nextState = "error";
-        break;
+        this.nextState = "error"
+        break
     }
   }
 
@@ -93,6 +97,6 @@ class Handshake {
         //`Length of string: ${this.stringLength}\n` +
         `Server + port: ${this.ip}:${this.port}\n` +
         `Next State: ${this.nextState}\n`,
-    );
+    )
   }
 }
